50161efe7dbf8b81cce7182ddecc310588ce6761
Bob McAleavey <bobmcaleavey@gmail.com>
2021-03-19 15:55:00 -0400

    initial commit

---
+++ src/server.js
@@ -0,0 +1,15 @@
+// libraries
+import compression from 'compression';
+import polka from 'polka';
+import * as sapper from '@sapper/server';
+
+const { PORT } = process.env;
+
+polka() // You can also use Express
+  .use(
+		compression({ threshold: 0 }), 
+		sapper.middleware()
+	)
+  .listen(PORT, err => {
+    if (err) console.log('error', err);
+  });
