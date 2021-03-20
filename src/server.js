e7a5955b3b2c4165a4691408ac48233d69f0b2da
bobthered <bobmcaleavey@gmail.com>
2021-03-20 07:00:11 -0400

    add tailwindcss

--- src/server.js
+++ src/server.js
@@ -3,16 +3,15 @@ import compression from 'compression';
 import express from 'express';
 import * as sapper from '@sapper/server';
 
+// initiate express app;
+const app = express();
+
 // env variables
 const { PORT } = process.env;
 
 // express server
 const server = app
-	.use(
-		compression({threshold: 0}),
-		express.json(),
-		sapper.middleware()
-	)
-	.listen(PORT || 90, () => {
-		console.log(`Listening at http://localhost:${PORT || 80}`)
-	})
+  .use(compression({ threshold: 0 }), express.json(), sapper.middleware())
+  .listen(PORT || 90, () => {
+    console.log(`Listening at http://localhost:${PORT || 80}`);
+  });
