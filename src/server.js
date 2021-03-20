03705ccfbfc58efa8620b9b7ddfccc9a2f8ad384
bobthered <bobmcaleavey@gmail.com>
2021-03-20 07:29:08 -0400

    add sirv

--- src/server.js
+++ src/server.js
@@ -2,16 +2,23 @@
 import compression from 'compression';
 import express from 'express';
 import * as sapper from '@sapper/server';
+import sirv from 'sirv';
 
 // initiate express app;
 const app = express();
 
 // env variables
-const { PORT } = process.env;
+const { NODE_ENV, PORT } = process.env;
+const dev = NODE_ENV === 'development';
 
 // express server
 const server = app
-  .use(compression({ threshold: 0 }), express.json(), sapper.middleware())
+  .use(
+    compression({ threshold: 0 }),
+    sirv('public', { dev }),
+    express.json(),
+    sapper.middleware()
+  )
   .listen(PORT || 90, () => {
     console.log(`Listening at http://localhost:${PORT || 80}`);
   });
