d81b576e1a363b28a10773003da59d4effc9848f
bobthered <bobmcaleavey@gmail.com>
2021-03-21 10:26:10 -0400

    add init logic

---
+++ src/components/Init/index.js
@@ -0,0 +1,4 @@
+// components
+import Component from './Component.svelte';
+
+export { Component };
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,10 @@
 <script>
+	// components
+	import { Component as Init } from '@components/Init';
 </script>
 
 <svelte:head>
 	<title>Sapper Boilerplate</title>
 </svelte:head>
+
+<Init />
