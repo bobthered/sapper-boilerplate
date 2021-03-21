ca069ba5a06b2b4f519bea16f6153d2ea398798f
bobthered <bobmcaleavey@gmail.com>
2021-03-21 07:05:02 -0400

    reset index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,8 @@
 <script>
-	// components
-	import Landing01 from './templates/Landing01.svelte';
 </script>
 
 <svelte:head>
 	<title>Sapper project template</title>
 </svelte:head>
 
-<Landing01 />
-<Landing01 />
+Home
