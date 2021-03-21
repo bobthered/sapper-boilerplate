34c47525ea4afcd7b98f6a4e7a7e448b7e72cab9
bobthered <bobmcaleavey@gmail.com>
2021-03-21 10:26:22 -0400

    refactor for asynchronous

--- src/server.js
+++ src/server.js
@@ -5,25 +5,34 @@ import express from 'express';
 import mongoose from './mongoose';
 import * as sapper from '@sapper/server';
 import sirv from 'sirv';
+import socketio from '@socket.io-server';
 
-// initiate express app;
-const app = express();
+// init script
+const init = async () => {
+  // initiate express app;
+  const app = express();
 
-// initiate mongoose
-mongoose.init();
+  // initiate mongoose
+  await mongoose.init();
 
-// env variables
-const { NODE_ENV, PORT } = process.env;
-const dev = NODE_ENV === 'development';
+  // env variables
+  const { NODE_ENV, PORT } = process.env;
+  const dev = NODE_ENV === 'development';
 
-// express server
-const server = app
-  .use(
-    compression({ threshold: 0 }),
-    sirv('public', { dev }),
-    express.json(),
-    sapper.middleware()
-  )
-  .listen(PORT || 90, () => {
-    console.log(`Listening at http://localhost:${PORT || 80}`);
-  });
+  // express server
+  const server = app
+    .use(
+      compression({ threshold: 0 }),
+      sirv('public', { dev }),
+      express.json(),
+      sapper.middleware()
+    )
+    .listen(PORT || 90, () => {
+      console.log(`Listening at http://localhost:${PORT || 80}`);
+    });
+
+  // initiate socket.io
+  socketio.init(server);
+};
+
+init();
